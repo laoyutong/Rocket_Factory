b676ba0e628e853c6a9be0e499be7d3d258dd44d
laoyutong <1262145400@qq.com>
2021-11-14 17:43:28 +0800

    feat: redux-toolkit add createApi

--- redux-toolkit/index.js
+++ redux-toolkit/index.js
@@ -1,6 +1,8 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import produce from "immer";
+import { useContext, useEffect, useState } from "react";
+import { ReactReduxContext } from "react-redux";
 
 export const configureStore = ({
   reducer,
@@ -72,4 +74,73 @@ export const createAsyncThunk = (type, payloadCreator) => {
     fulfilled,
     rejected,
   });
+};
+
+export const fetchBaseQuery = ({ baseUrl }) => {
+  return async function (url) {
+    return await fetch(baseUrl + url).then((res) => res.json());
+  };
+};
+
+const FETCH_DATA = "FETCH_DATA";
+
+export const createApi = ({ reducerPath, baseQuery, endpoints }) => {
+  const { reducer, actions } = createSlice({
+    name: reducerPath,
+    initialState: { data: undefined, error: undefined, isLoading: false },
+    reducer: {
+      setValue(state, action) {
+        Object.keys(action.payload).forEach((k) => {
+          state[k] = action.payload[k];
+        });
+      },
+    },
+  });
+
+  const middleware = ({ dispatch }) => {
+    return (next) => {
+      return (action) => {
+        if (action.type === FETCH_DATA) {
+          (async () => {
+            dispatch(actions.setValue({ isLoading: true }));
+            try {
+              const data = await baseQuery(action.payload.url);
+              dispatch(actions.setValue({ isLoading: false, data }));
+            } catch (error) {
+              dispatch(actions.setValue({ isLoading: false, error }));
+            }
+          })();
+        } else {
+          next(action);
+        }
+      };
+    };
+  };
+
+  const builder = {
+    query(options) {
+      return {
+        useQuery: (arg) => {
+          const { store } = useContext(ReactReduxContext);
+          const [, forceUpdate] = useState([]);
+          useEffect(() => {
+            const unSubscribe = store.subscribe(() => forceUpdate([]));
+            store.dispatch({
+              type: FETCH_DATA,
+              payload: { url: options.query(arg) },
+            });
+            return unSubscribe;
+          }, []);
+          return store.getState()[reducerPath];
+        },
+      };
+    },
+  };
+
+  return {
+    reducerPath,
+    reducer,
+    middleware,
+    endpoints: endpoints(builder),
+  };
 };
